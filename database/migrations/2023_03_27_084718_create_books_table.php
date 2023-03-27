c07c4a0892ef49498ab2972907609b3fd9286306
Andrea <e@mail>
2023-03-27 12:46:07 +0300

    books migration created

---
+++ database/migrations/2023_03_27_084718_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('book_id');
+            $table->string('title');
+            $table->text('summary');
+            $table->integer('edition');
+            $table->string('placement');
+            $table->timestamp('edited_at');
+            $table->integer('isbn');
+            $table->integer('number_of_copies');
+            $table->integer('rented_copies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
