49819b47d57790ae2af94ae14abbae5fb2c73a08
Andrea <e@mail>
2023-03-29 15:59:25 +0300

    db seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Genre;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -17,10 +20,18 @@ class DatabaseSeeder extends Seeder
     {
         DB::table('users')->insert([
             'username' => 'admin',
-            'firstname' => 'Admin',
-            'lastname' => 'Admin',
             'email' => 'admin@argon.com',
-            'password' => bcrypt('secret')
+            'password' => bcrypt('secret'),
+            'edited_at' => now()
         ]);
+
+        Book::factory(15)->create();
+        Author::factory(5)->create();
+
+        $author = Author::factory()->create();
+
+        $scifi = Genre::factory()->create(['name' => 'sci-fi']);
+        $reallife = Genre::factory()->create(['name' => 'real life']);
+        $fantasy = Genre::factory()->create(['name' => 'fantasy']);
     }
 }
