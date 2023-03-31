caf8c7416aa209dc27ab7bcea77d8616d01f2623
Andrea <e@mail>
2023-03-31 14:44:19 +0300

    topnav simplified

--- resources/views/layouts/navbars/auth/topnav.blade.php
+++ resources/views/layouts/navbars/auth/topnav.blade.php
@@ -11,25 +11,6 @@
             <h6 class="font-weight-bolder text-white mb-0">{{ $title }}</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-            <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                <div class="input-group">
-                    <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
-                    <input type="text" class="form-control" placeholder="Type here...">
-                </div>
-            </div>
-
-
-
-            <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                <form method="GET" action="">
-                    @if (request('genres'))
-                        <input type="hidden" name="genres" value="{{ request('genres') }}">
-                    @endif
-
-                    <input type="text" name="search" placeholder="search..." 
-                    class="form-control" value="{{ request('search') }}">
-                </form>
-            </div>
 
             <ul class="navbar-nav  justify-content-end">
                 <li class="nav-item d-flex align-items-center">
