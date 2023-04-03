6472895e98934fd3deddbee7520f0dc089042cea
Andrea <e@mail>
2023-04-03 20:52:46 +0300

    book-comment component created

---
+++ resources/views/components/book-comment.blade.php
@@ -0,0 +1,25 @@
+@props(['comment'])
+
+<div class="container-fluid py-1">
+    <div class="row">
+        <div class="col-md-16 mt-4">
+            <div class="card">
+                <div class="container">
+                    <div> 
+                        <header>
+                            <h6 class="">{{ $comment->user->username }}</h6>
+                            <p class="text-xs">
+                                posted
+                                <time> {{ $comment->created_at }} </time>
+                            </p>
+                        </header>
+
+                        <p>
+                            {{ $comment->comment }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
