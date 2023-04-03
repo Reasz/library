66d9974bb678848d44afca96fe7da88a7f1d7abc
Andrea <e@mail>
2023-04-03 20:53:08 +0300

    routes updated for comments

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\ResetPassword;
 use App\Http\Controllers\ChangePassword;
+use App\Http\Controllers\BookCommentsController;
 
 use App\Http\Controllers\BookController;
 
@@ -48,8 +49,11 @@ Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('gue
 Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');
 Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
+
 Route::get('/books', [BookController::class, 'books'])->name('books');
 Route::get('/books-{book:id}', [BookController::class, 'book'])->name('book');
+Route::post('/books-{book:id}-comments', [BookCommentsController::class, 'store']);
+
 
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
@@ -61,5 +65,6 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static');
 	Route::get('/{page}', [PageController::class, 'index'])->name('page');
 	Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
 
 });
