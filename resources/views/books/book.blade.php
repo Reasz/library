e791ffbc1a13f9be7c3e6271fd903d2fd97c16b8
Andrea <e@mail>
2023-04-04 21:45:05 +0300

    book view updated

--- resources/views/books/book.blade.php
+++ resources/views/books/book.blade.php
@@ -64,24 +64,10 @@
             </div>
         </div>
     </div>
+
+
+    @include ('books._add-comment-form')
     
-    <div class="container-fluid py-4">
-        <div class="row">
-            <div class="col-md-16 mt-4">
-                <div class="card">
-                    <form method="POST" action="/books-{{ $book->id }}-comments">
-                        @csrf
-                        <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Example textarea</label>
-                            <textarea name="comment" class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="type..."></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button class="btn" type="submit">Post</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
     
     @foreach ($book->comments as $comment)
         <x-book-comment :comment="$comment" />
