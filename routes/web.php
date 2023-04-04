2a27928ab855920d5c96b93047d7a1198ce129fd
Andrea <e@mail>
2023-04-04 21:44:45 +0300

    route added for adding books

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,8 @@ Route::get('/books', [BookController::class, 'books'])->name('books');
 Route::get('/books-{book:id}', [BookController::class, 'book'])->name('book');
 Route::post('/books-{book:id}-comments', [BookCommentsController::class, 'store']);
 
+Route::get('admin-book-create', [BookController::class, 'create'])->middleware('admin');
+Route::post('admin-books', [BookController::class, 'store'])->middleware('admin');
 
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
