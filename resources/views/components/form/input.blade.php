90f7286d6042a6bb8a525eeab238c865e2f6cb6b
Andrea <e@mail>
2023-04-05 23:09:33 +0300

    input component updated

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -3,7 +3,12 @@
 <div class="col-md-6">
     <div class="form-group">
         <label for="{{ $name }}">{{ ucwords($name) }}</label>
-        <input type="{{ $type }}" value="{{ old($name) }}" class="form-control" id="{{ $name }}" name="{{ $name }}" placeholder="{{ $name }}" required>
+        <input type="{{ $type }}"
+                class="form-control"
+                id="{{ $name }}"
+                name="{{ $name }}" 
+                placeholder="{{ $name }}" required
+                {{ $attributes([ 'value' => old($name)]) }}>
 
         <x-form.error name="{{ $name }}" />
     </div>
