8366009e12bc89c8b56e439abdde5a2adedc862f
Andrea <e@mail>
2023-04-05 16:24:53 +0300

    card component created

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,9 @@
+<div class="container-fluid py-4">
+    <div class="row">
+            <div class="col-md-16 mt-4">
+                <div class="card p-4">
+                    {{ $slot }}
+                </div>
+            </div>
+    </div>
+</div>
