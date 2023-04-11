f953b00972d4b9bdf31a39651a6ba88a3ed15ff1
Andrea <e@mail>
2023-04-11 12:31:26 +0300

    succes message updated

--- resources/views/admin/authors/create.blade.php
+++ resources/views/admin/authors/create.blade.php
@@ -4,15 +4,7 @@
     @include('layouts.navbars.auth.topnav', ['title' => 'Books']) 
 
     <x-card> 
-        @if (session('success'))
-            <div class="alert alert-primary alert-dismissible fade show" role="alert">
-                <span class="alert-icon"><i class="ni ni-like-2"></i></span>
-                <span class="alert-text"> {{ session('success') }} </span>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        @endif
+        <x-succes-message />
 
         <form method="POST" action="{{ route('admin-add-author') }}">
             @csrf
